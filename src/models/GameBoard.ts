fe717615cd0fb850528aaa84d7ca3af87826c5ac
Caleb Calderwood <Calderwoodc@gmail.com>
2025-02-28 17:44:53 -0700

    added check cell functionality and init board generation

--- src/models/GameBoard.ts
+++ src/models/GameBoard.ts
@@ -20,7 +20,21 @@ class GameBoard extends Phaser.GameObjects.Grid {
   /**
    * Generates a new game board (populates this.grid with cells)
    */
-  generateBoard() {}
+  generateBoard() {
+    for (let i = 0; i < this.width; i++) {
+      this.grid[i] = [];
+      for (let j = 0; j < this.height; j++) {
+        this.grid[i][j] = new Cell(
+          this.scene,
+          i * this.cellWidth,
+          j * this.cellHeight,
+          this.cellWidth,
+          this.cellHeight,
+          CellContent.EMPTY
+        );
+      }
+    }
+  }
 
   /**
    * Determines what to do when a cell is clicked
@@ -32,7 +46,33 @@ class GameBoard extends Phaser.GameObjects.Grid {
    * @param y y coordinate of the cell
    */
   checkCell(x: number, y: number): CellContent {
-    return CellContent.EMPTY;
+    const cell = this.grid[x][y];
+    if (cell.contains === CellContent.HAZARD) {
+      return CellContent.HAZARD;
+    }
+    if (cell.contains === CellContent.EMPTY) {
+      this.revealAdjacentCells(x, y);
+    }
+    return cell.contains;
+  }
+
+  /**
+   * Reveals all adjacent cells
+   * @param x x coordinate of the cell
+   * @param y y coordinate of the cell
+   */
+  revealAdjacentCells(x: number, y: number) {
+    const cell = this.grid[x][y];
+    if (cell.cellState === CellState.REVEALED) {
+      return;
+    }
+    cell.cellState = CellState.REVEALED;
+    if (cell.adjacentMines === 0) {
+      this.revealAdjacentCells(x - 1, y);
+      this.revealAdjacentCells(x + 1, y);
+      this.revealAdjacentCells(x, y - 1);
+      this.revealAdjacentCells(x, y + 1);
+    }
   }
 }
 

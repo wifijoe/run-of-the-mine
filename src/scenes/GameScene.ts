9cdf201c763f0acaf638d5ea9e513e2dc9d6d1e8
wifijoe <amhammondformal@gmail.com>
2025-04-07 04:04:51 -0600

    cleaned up gameScene and removed code duplication.

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -49,7 +49,6 @@ class GameScene extends Phaser.Scene {
       this.gameEndText.setOrigin(0.5);
       this.keydownListener = (event: KeyboardEvent) => {
         if (event.key === "r" || event.key === "R") {
-          this.basicDifficulty = [0, 0, 0];
           this.resetLevel();
         }
       };
@@ -58,6 +57,7 @@ class GameScene extends Phaser.Scene {
 
     this.events.on("levelComplete", () => {
       this.gameOver = true;
+      this.stopTimer();
       this.gameEndText = this.add.text(
         this.cameras.main.centerX,
         this.cameras.main.centerY,
@@ -74,7 +74,7 @@ class GameScene extends Phaser.Scene {
       this.keydownListener = (event: KeyboardEvent) => {
         if (event.key === "n" || event.key === "N") {
           this.basicDifficulty[0] += 1; // todo: make difficulty increase random
-          this.nextLevel();
+          this.resetLevel();
         }
       };
       this.input.keyboard?.on("keydown", this.keydownListener);
@@ -102,105 +102,9 @@ class GameScene extends Phaser.Scene {
 
     // Create new Level
     const cellSize = 32;
-    const boardWidth = 20;
-
-    this.add.image(0, 0, "dirt").setOrigin(0, 0);
-
-    const x =
-      (this.game.config.width as number) / 2 - cellSize * (boardWidth / 2);
-    const y =
-      (this.game.config.height as number) / 2 - cellSize * (boardWidth / 2);
-
-    this.level = new Level(
-      this,
-      x,
-      y,
-      boardWidth,
-      boardWidth,
-      cellSize,
-      cellSize,
-      0
-    );
-    this.add.existing(this.level.board);
-    this.level.startLevel();
-
-    // Center
-    const centerX = this.cameras.main.width / 2;
-    // const centerY = this.cameras.main.height / 2;
-
-    // Title of game
-    this.add.text(40, 15, "Run of the Mine", {
-      fontSize: "30px",
-      color: "#ffffff",
-      fontFamily: '"Orbitron", sans-serif',
-    });
-
-    // Game score, currently not synced with anything
-    this.scoreText = this.add
-      .text(centerX * 2 - 40, 30, "Score: 0", {
-        fontSize: "30px",
-        color: "#ffffff",
-        fontFamily: '"Orbitron", sans-serif',
-      })
-      .setOrigin(1, 0);
-
-    // Timer
-    const { width, height } = this.scale;
-
-    this.totalTime = 150; // maybe change?
-    this.timeLeft = this.totalTime;
-
-    // Position the bar centered at the bottom
-    this.barX = width / 2 - this.barWidth / 2 - 13;
-    this.barY = height - 50;
-
-    // Create a background bar
-    this.outlineBar = this.add.graphics();
-    this.outlineBar.lineStyle(3, 0xffffff);
-    this.outlineBar.strokeRect(
-      this.barX - 3,
-      this.barY - this.barHeight / 2 - 3,
-      this.barWidth + 3,
-      this.barHeight + 6
-    );
-
-    // Create a timer bar
-    this.timerBar = this.add.graphics();
-    this.updateTimerBar();
-
-    this.countdownEvent = this.time.addEvent({
-      delay: 80,
-      callback: this.updateCountdown,
-      callbackScope: this,
-      loop: true,
-    });
-  }
-
-  nextLevel() {
-    this.gameOver = false;
-    if (this.level) {
-      this.level.board.destroy();
-    }
-
-    // Remove other game objects
-    if (this.gameEndText) {
-      this.gameEndText.destroy();
-    }
-
-    // Remove the event listener
-    this.input.keyboard?.off("keydown", this.keydownListener);
-
-    /*   // Reset other game objects
-    if (this.scoreText) {
-      this.scoreText.setText("Score: 0");
-    }
-    */
-
-    // Create new Level
-    const cellSize = 32;
     const boardWidth = 20 + this.basicDifficulty[0];
 
-    this.add.image(0, 0, "dirt").setOrigin(0, 0);
+    this.add.image(0, 0, "dirt").setOrigin(0, 0).setDisplaySize(this.scale.width, this.scale.height);
 
     const x =
       (this.game.config.width as number) / 2 - cellSize * (boardWidth / 2);
@@ -243,7 +147,7 @@ class GameScene extends Phaser.Scene {
     // Timer
     const { width, height } = this.scale;
 
-    this.totalTime = 150; // maybe change?
+    this.totalTime = 300; // maybe change?
     this.timeLeft = this.totalTime;
 
     // Position the bar centered at the bottom
@@ -283,7 +187,7 @@ class GameScene extends Phaser.Scene {
     if (this.timeLeft <= 0) {
       this.timeLeft = 0;
       this.stopTimer();
-      this.scene.start("GameOver");
+      this.level.board.loseGame();
     }
 
     this.updateTimerBar();
